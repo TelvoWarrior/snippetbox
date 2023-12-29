9267d3b777a010fb6a8f137fb94b66af69a808f5
Telvo <telvowarrior@gmail.com>
2023-12-29 17:11:15 +0300

    replace w.WriteHeader() and w.Write with http.Error()

--- main.go
+++ main.go
@@ -20,12 +20,8 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
-		// Use the Header().Set() method to add an 'Allow: POST' header to the
-		// response header map. The first parameter is the header name, and
-		// the second parameter is the header value.
 		w.Header().Set("Allow", "POST")
-		w.WriteHeader(405)
-		w.Write([]byte("Method Not Allowed"))
+		http.Error(w, "Method Not Allowed", 405)
 		return
 	}
 
